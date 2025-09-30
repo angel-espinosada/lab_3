0f27e48b65e220b91d1f9ddc20e0e99a9bf82377
Angel Espinosa David <angel.espinosa@udea.edu.co>
2025-09-29 20:55:33 -0500

    Proyecto nuevo

---
+++ Lab_3/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    {
+        int opcion=1;
+        while(opcion!=0)
+        {
+            cout<<"Menu de incio"<<endl;
+            cout<<"1- Codificacion de archivos"<<endl;
+            cout<<"2- Decodificacion de archivos"<<endl;
+            cout<<"3-Cajero automatico"<<endl;
+            cout<<"0- para salir"<<endl;
+            cin>>opcion;
+
+            switch (opcion) {
+
+            case 1:{
+
+            }
+            break;
+                break;
+            default:{
+
+                if(opcion!=0)
+                    cout<<"opcion no valida"<<endl;
+                break;
+            }
+            }
+        }
+    }
+        cout << "Hello World!" << endl;
+
+        return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
