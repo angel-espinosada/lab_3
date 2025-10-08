b43fefe85353e2386714a3f48a6cdbc4f66d0988
Angel Espinosa David <angel.espinosa@udea.edu.co>
2025-10-07 21:05:52 -0500

    Actualizando repositorio

--- Lab_3/main.cpp
+++ Lab_3/main.cpp
@@ -23,6 +23,12 @@ int main()
             switch (opcion) {
 
             case 1:{
+                string arch_user;
+                int semilla;
+                cout<<"Ingrese el nombre del archivo a codificar"<<endl;
+                cin>>arch_user;
+                cout<<"Con que se semilla quieres codidifcar"<<endl;
+                cin>>semilla;
 
             }
             break;
@@ -45,24 +51,23 @@ int main()
 
 
 char* leerArchivo_Original(const char* nombre) {
-    ifstream archivo(nombre, ios::binary); // modo binario para preservar todo
+    ifstream archivo(nombre, ios::binary);
     if (!archivo.is_open()) {
         cerr << "Error: No se pudo abrir el archivo '" << nombre << "'\n";
         return nullptr;
     }
-
     // Ir al final para saber el tamaño
     archivo.seekg(0, ios::end);
     streampos tam = archivo.tellg();
     archivo.seekg(0, ios::beg);
 
-    // Reservar memoria exacta (+1 para '\0' si quieres tratarlo como cadena)
-    char* buffer = new char[tam + 1];
-    archivo.read(buffer, tam);
-    buffer[tam] = '\0'; // opcional: solo si vas a usarlo como cadena C
+    // Reservar memoria dinamica
+    char* tex_temp = new char[tam + 1];
+    archivo.read(tex_temp, tam);
+    tex_temp[tam] ='\0';
 
     archivo.close();
-    return buffer; // ¡Recuerda liberar con delete[]!
+    return tex_temp;
 }
 
 
