78694fc9a3a18a5ab85964cc2415e61580ec850b
Angel Espinosa David <angel.espinosa@udea.edu.co>
2025-10-20 23:11:14 -0500

    Cajero aumatomatico codigo hecho en clase 14/10/2025

--- main.cpp
+++ main.cpp
@@ -3,6 +3,143 @@
 #include <string>
 using namespace std;
 
+float sumar(float saldo, float consignacion);
+float restar(float saldo, float retiro);
+
+string leerArchivo(const string& nombre);
+void guardarArchivo(const string& nombre, const string& contenido);
+string codificar(const string& texto);
+string decodificar(const string& texto);
+int main() {
+
+    {
+        int opcion=1;
+        while(opcion!=0)
+        {
+            cout<<"Menu de incio"<<endl;
+            cout<<"1- Codificacion de archivos"<<endl;
+            cout<<"2- Decodificacion de archivos"<<endl;
+            cout<<"3-Cajero automatico"<<endl;
+            cout<<"0- para salir"<<endl;
+            cin>>opcion;
+
+            switch (opcion) {
+
+            case 1:{
+
+
+            }
+
+            case 2:{
+
+                cout<<"Desencriptar"<<endl;
+
+
+            }
+
+            break;
+
+
+
+            case 3:{
+                int identificacion;
+                float plata, resul;
+                float consignar;
+                float retirar;
+                string clave;
+
+                cout<<"::Cajero::"<<endl;
+                plata=100000;
+                string pista="ala";
+                int identificacion1=12345;
+                cout<<"Ingrese su cedula"<<endl;
+                cin>>identificacion;
+                cout<<"Ingrese la clave"<<endl;
+                cin>>clave;
+                //if(pista!=clave&&identificacion!=identificacion1)
+
+                if(identificacion!=identificacion1){
+                    cout<<"Error en la identificacion"<<endl;
+                }
+                if(pista!=clave){
+                    cout<<"Error en la clave"<<endl;
+                }
+
+
+                else{
+
+                    int opcion=1;
+                    while(opcion!=0)
+                    {
+                        cout<<":::Bienvenido:::"<< identificacion<<endl;
+                        cout<<"1- Consultar saldo"<<endl;
+                        cout<<"2- Retirar"<<endl;
+                        cout<<"3-Consignar"<<endl;
+                        cout<<"0- para salir"<<endl;
+                        cin>>opcion;
+
+                        switch (opcion) {
+
+                        case 1:{
+                            cout<<"Su saldo es:"<<plata<<endl;
+
+
+                        }
+
+                        break;
+
+                        case 2:
+                        {
+                            cout<<"Cuanto deseas retirar: "<<endl;
+
+                            cin>>retirar;
+
+                            resul=restar(plata, retirar);
+                            cout<<"Se retiro "<<retirar<<" Su saldo es: "<<resul-1000<<endl;
+
+
+                        }
+                        break;
+
+                        case 3:{
+
+                            cout<<"Cuanto desear consignar: "<<endl;
+                            cin>>consignar;
+
+                            resul=sumar(plata,consignar);
+
+                            cout<<"Vas a consignar  "<<consignar<<" su saldo es: "<<resul<<endl;
+                        }
+
+                        default:{
+
+                            if(opcion!=0)
+                                cout<<"opcion no valida"<<endl;
+                            break;
+                        }
+
+                        }
+                    }
+                }
+            }
+            break;
+
+
+
+            default:{
+
+                if(opcion!=0)
+                    cout<<"opcion no valida"<<endl;
+                break;
+            }
+            }
+        }
+    }
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
+
 // Lee texto desde un archivo
 string leerArchivo(const string& nombre) {
     ifstream archivo(nombre);
@@ -39,49 +176,15 @@ string decodificar(const string& texto) {
     return codificar(texto); // invertir 2 veces recupera el original
 }
 
-int main() {
-    int opcion = 1;
-    while (opcion != 0) {
-        cout << "\n--- Menu ---\n";
-        cout << "1. Codificar archivo\n";
-        cout << "2. Decodificar archivo\n";
-        cout << "0. Salir\n";
-        cout << "Opcion: ";
-        cin >> opcion;
+float sumar(float saldo, float consignacion){
 
-        if (opcion == 1) {
-            string entrada, salida;
-            cout << "Archivo de entrada: ";
-            cin >> entrada;
-            cout << "Archivo de salida: ";
-            cin >> salida;
+    saldo = saldo+consignacion;
 
-            string texto = leerArchivo(entrada);
-            string codificado = codificar(texto);
-            guardarArchivo(salida, codificado);
-
-            cout << "Archivo codificado guardado como: " << salida << endl;
-        }
-        else if (opcion == 2) {
-            string entrada, salida;
-            cout << "Archivo codificado (entrada): ";
-            cin >> entrada;
-            cout << "Archivo decodificado (salida): ";
-            cin >> salida;
-
-            string texto = leerArchivo(entrada);
-            string decodificado = decodificar(texto);
-            guardarArchivo(salida, decodificado);
-
-            cout << "Archivo decodificado guardado como: " << salida << endl;
-        }
-        else if (opcion != 0) {
-            cout << "Opcion no valida.\n";
-        }
-    }
-
-    cout << "Programa finalizado.\n";
-    return 0;
+    return saldo;
 }
+float restar(float saldo, float retiro){
 
+    saldo=saldo-retiro;
 
+    return saldo;
+}
